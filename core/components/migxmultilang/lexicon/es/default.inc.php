31eeaf39172b7526c8aa0eba48474c348e1c8119
Abel Martínez <guelu63@gmail.com>
2020-10-19 13:03:08 +0200

    translated

---
+++ core/components/migxmultilang/lexicon/es/default.inc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * migxmultilang
+ *
+ * @package migxmultilang
+ * @language es
+ */
+
+//$_lang['mig_'] = '';
+
+$_lang['mml.language'] = 'Lenguaje';
+$_lang['mml.import_example_config'] = 'Importar configuraciones';
+$_lang['mml.add_config'] = 'Añadir pestañaal formulario de configuración';
+$_lang['mml.create_tvs'] = 'Crear VdP\'s';
+$_lang['mml.published'] = 'Publicado';
+$_lang['mml.to_translate'] = 'Para traducir';
+$_lang['mml.specific_templates'] = 'Plantillas específicas';
+$_lang['mml.specific_templates_desc'] = 'Cada plantilla puede tener solo una pestaña de idioma. ¡Esto elimina las plantillas seleccionadas de otras pestañas de formulario!';
+$_lang['mml.use_as_default_formtabs'] = 'Usar como pestañas de formulario predeterminadas para todas las demás plantillas';
+$_lang['mml.hide_default_lang'] = 'Ocultar idioma predeterminado de la cuadrícula de Traducciones';
+$_lang['mml.hide_default_lang_fields'] = 'Ocultar algunos campos del idioma predeterminado';
+$_lang['mml.hide_default_lang_fields_desc'] = 'Lista de nombres de campo separados por comas';
+$_lang['mml.order_position'] = 'Posición';
