3ef417d7d32d77343daeb1165840cd38fb9c5fb6
Abel Martínez <guelu63@gmail.com>
2020-10-19 13:16:21 +0200

    translated

--- core/components/migxmultilang/lexicon/es/default.inc.php
+++ core/components/migxmultilang/lexicon/es/default.inc.php
@@ -3,21 +3,21 @@
  * migxmultilang
  *
  * @package migxmultilang
- * @language es
+ * @language en
  */
 
 //$_lang['mig_'] = '';
 
-$_lang['mml.language'] = 'Lenguaje';
-$_lang['mml.import_example_config'] = 'Importar configuraciones';
-$_lang['mml.add_config'] = 'Añadir pestañaal formulario de configuración';
-$_lang['mml.create_tvs'] = 'Crear VdP\'s';
-$_lang['mml.published'] = 'Publicado';
-$_lang['mml.to_translate'] = 'Para traducir';
-$_lang['mml.specific_templates'] = 'Plantillas específicas';
-$_lang['mml.specific_templates_desc'] = 'Cada plantilla puede tener solo una pestaña de idioma. ¡Esto elimina las plantillas seleccionadas de otras pestañas de formulario!';
-$_lang['mml.use_as_default_formtabs'] = 'Usar como pestañas de formulario predeterminadas para todas las demás plantillas';
-$_lang['mml.hide_default_lang'] = 'Ocultar idioma predeterminado de la cuadrícula de Traducciones';
-$_lang['mml.hide_default_lang_fields'] = 'Ocultar algunos campos del idioma predeterminado';
-$_lang['mml.hide_default_lang_fields_desc'] = 'Lista de nombres de campo separados por comas';
-$_lang['mml.order_position'] = 'Posición';
+$_lang['mml.language'] = 'Language';
+$_lang['mml.import_example_config'] = 'Import Configurations';
+$_lang['mml.add_config'] = 'Add Formtabs Configuration';
+$_lang['mml.create_tvs'] = 'Create TVs';
+$_lang['mml.published'] = 'Published';
+$_lang['mml.to_translate'] = 'To translate';
+$_lang['mml.specific_templates'] = 'Specific Templates';
+$_lang['mml.specific_templates_desc'] = 'Each Template can have only one language-formtabs. This does remove selected templates from other formtabs!';
+$_lang['mml.use_as_default_formtabs'] = 'Use as Default Formtabs for all other templates';
+$_lang['mml.hide_default_lang'] = 'Hide default language from Translations - grid';
+$_lang['mml.hide_default_lang_fields'] = 'Hide some fields from default language';
+$_lang['mml.hide_default_lang_fields_desc'] = 'Comma - separated list of fieldnames';
+$_lang['mml.order_position'] = 'Position';
